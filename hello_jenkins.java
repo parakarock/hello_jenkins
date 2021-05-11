00bb6e90d848ec141808844acc1bc093c686b61d
NeMo <friend-opp_2010@hotmail.com>
2021-05-11 17:43:27 +0700

    second commit

--- hello_jenkins.java
+++ hello_jenkins.java
@@ -1,5 +1,5 @@
 public class Hello {
-    public static void public static void main(String[] args) {
+    public static void  main(String[] args) {
         System.ot.println("Hello_jenkins by NeMo")
     }
 }
